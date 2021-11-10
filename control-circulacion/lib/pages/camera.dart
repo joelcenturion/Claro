dee38d44593502a566b95df4c01979cd603879e7
Joel Centurion <jcenturion@bypersoft.com.py>
2021-11-10 11:59:50 -0300

    10-11-2021

--- control-circulacion/lib/pages/camera.dart
+++ control-circulacion/lib/pages/camera.dart
@@ -24,7 +24,7 @@ class _CameraState extends State<Camera> {
   };
   Future<void> faceRecognition() async {
     try {
-      String url = 'http://192.168.100.73:8080/';
+      String url = 'http://190.104.149.238:8080/';
       http.Response response = await http.post(
         Uri.parse(url),
         headers: <String, String>{
@@ -56,7 +56,7 @@ class _CameraState extends State<Camera> {
 
     Uint8List landmark_data;
     try {
-      String url = 'http://192.168.100.73:8081/';
+      String url = 'http://190.104.149.238:8081/';
       http.Response response = await http.post(
         Uri.parse(url),
         headers: <String, String>{
@@ -118,7 +118,7 @@ class _CameraState extends State<Camera> {
 
       File imageTemporary = File(image.path);
       final image1 = img.decodeImage(File(image.path).readAsBytesSync())!;
-      final thumbnail = img.copyResize(image1, width: 145, height: 180);
+      final thumbnail = img.copyResize(image1, width: 135, height: 180);
       imageTemporary.writeAsBytes(img.encodePng(thumbnail));
       setState(() {
         this.image = imageTemporary;
@@ -151,105 +151,4 @@ class _CameraState extends State<Camera> {
       body: Center(child: CircularProgressIndicator()),
     );
   }
-
-  // showImage() {
-  //   if (displayImage != null) {
-  //     return Scaffold(
-  //       backgroundColor: Colors.grey[300],
-  //       appBar: AppBar(
-  //         backgroundColor: Colors.indigo[800],
-  //         title: Text(
-  //           'Reconocimiento Facial',
-  //           style: GoogleFonts.irishGrover(fontSize: 20, letterSpacing: 2.0),
-  //         ),
-  //       ),
-  //       body: Center(
-  //         child: FractionallySizedBox(
-  //           widthFactor: 1,
-  //           heightFactor: 1,
-  //           child: Container(
-  //             margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-  //             child: ClipRRect(
-  //               borderRadius: BorderRadius.circular(20),
-  //               child: Image.memory(
-  //                 displayImage!,
-  //                 fit: BoxFit.fill,
-  //               ),
-  //             ),
-  //           ),
-  //         ),
-  //       ),
-  //     );
-  //   } else {
-  //     return Center(child: CircularProgressIndicator());
-  //   }
-  // }
-
-  // showAlert(bool input) {
-  //   return showDialog(
-  //       context: context,
-  //       builder: (BuildContext context) {
-  //         Future.delayed(Duration(milliseconds: 700), () {
-  //           Navigator.pop(context);
-  //         });
-  //         var screen = MediaQuery.of(context).size;
-  //         if (input) {
-  //           return Container(
-  //             child: AlertDialog(
-  //               backgroundColor: Colors.green,
-  //               title: Row(
-  //                 mainAxisAlignment: MainAxisAlignment.center,
-  //                 children: [
-  //                   Icon(
-  //                     Icons.done_outlined,
-  //                     color: Colors.white,
-  //                     size: 30,
-  //                   ),
-  //                   Text(
-  //                     '   Coincidente',
-  //                     style: TextStyle(color: Colors.white, fontSize: 15),
-  //                   ),
-  //                 ],
-  //               ),
-  //               shape: RoundedRectangleBorder(
-  //                   borderRadius: BorderRadius.circular(100)),
-  //               insetPadding: EdgeInsets.only(
-  //                   bottom: screen.height * 0.8,
-  //                   left: screen.width * 0.2,
-  //                   right: screen.width * 0.2),
-  //               titlePadding: EdgeInsets.symmetric(vertical: 13),
-  //             ),
-  //           );
-  //         } else {
-  //           return Container(
-  //             decoration: BoxDecoration(
-  //                 borderRadius: BorderRadius.all(Radius.circular(20.0))),
-  //             child: AlertDialog(
-  //               backgroundColor: Colors.red,
-  //               title: Row(
-  //                 mainAxisAlignment: MainAxisAlignment.center,
-  //                 children: [
-  //                   Icon(
-  //                     Icons.close_outlined,
-  //                     color: Colors.white,
-  //                     size: 30,
-  //                   ),
-  //                   Text(
-  //                     '   NO Coincidente',
-  //                     style: TextStyle(color: Colors.white, fontSize: 15),
-  //                   ),
-  //                 ],
-  //               ),
-  //               shape: RoundedRectangleBorder(
-  //                   borderRadius: BorderRadius.circular(100)),
-  //               insetPadding: EdgeInsets.only(
-  //                   bottom: screen.height * 0.8,
-  //                   left: screen.width * 0.2,
-  //                   right: screen.width * 0.2),
-  //               titlePadding: EdgeInsets.symmetric(vertical: 13),
-  //             ),
-  //           );
-  //         }
-  //       });
-  // }
 }
